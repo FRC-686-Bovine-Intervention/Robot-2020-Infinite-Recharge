e777455f6adb36142e0a949aa5946665c5cb43da
Bryn Kerney <brynk2003@gmail.com>
2020-02-27 16:01:57 -0500

    control scheme edits

--- src/main/java/frc/robot/lib/joystick/DriverControlsThrustmasterRight.java
+++ src/main/java/frc/robot/lib/joystick/DriverControlsThrustmasterRight.java
@@ -62,6 +62,11 @@ public class DriverControlsThrustmasterRight extends DriverControlsBase
         {
             case SHOOT:                         return !controller.getButton(Thrustmaster.kTopButton3) && controller.getButton(Thrustmaster.kTriggerButton) || controller.getButton(Thrustmaster.kBottomThumbButton);
             case DRIVE_ASSIST:                  return controller.getButton(Thrustmaster.kLeftThumbButton);
+            case INTAKE_TOGGLE:                 return controller.getButton(Thrustmaster.kRightThumbButton);
+            case INTAKE_STORED:                 return controller.getButton(Thrustmaster.kTopButton5);
+            case LOCK_LIFT:                 return controller.getButton(Thrustmaster.kTopButton1);
+            case UNLOCK_LIFT:                 return controller.getButton(Thrustmaster.kTopButton2);
+            case TOGGLE_PTO:                 return controller.getButton(Thrustmaster.kTopButton4);
             case QUICK_TURN:                    return false;
             default:                            return false;
         }
